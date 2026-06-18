d8edfedfd89c5fbc379fa350d47c9c36742c0d3d
Jae Hyoun Park <pjhyoun01@gmail.com>
2026-06-18 15:25:22 +0900

    Update TaskItems.jsx

--- hello-react-context/src/components/todo/TaskItems.jsx
+++ hello-react-context/src/components/todo/TaskItems.jsx
@@ -3,13 +3,13 @@ import {useContext, useRef} from "react";
 import {TodoContext} from "./context/TodoContext.jsx";
 
 const TaskItems = ({id, priorities}) => {
-    const {getTodo, done} = useContext(TodoContext);
+    const {getTodo, done2} = useContext(TodoContext);
     // props todo의 이름과 todo.todo의 이름이 같아 객체 구조 분해 불가
     // todo.todo의 이름을 todoTask로 변경해 할당
     const {id: todoId, todo: todoTask, dueDate, priority, done} = getTodo(id)
 
 
-    const doneClass = done ? 'done' : 'not-done';
+    const doneClass = done2 ? 'done' : 'not-done';
 
     const checkboxRef = useRef();
     const confirmRef = useRef();
@@ -24,7 +24,7 @@ const TaskItems = ({id, priorities}) => {
     }
 
     const onConfirmOkHandler = () => {
-        done(todoId);
+        done2(todoId);
     }
 
     const onConfirmCancelHandler = () => {
